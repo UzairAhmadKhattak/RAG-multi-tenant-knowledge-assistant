db3dc1016a07c2b2c7f139ebeea063b361793777
Uzair Ahmad <uzair3368@gmail.com>
2026-05-02 17:45:18 +0500

    add login route

--- src/app/api/v1/auth.py
+++ src/app/api/v1/auth.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,HTTPException
+from src.app.schemas.user import UserLoginRequest, UserLoginResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.app.db.session import get_db
+from src.app.services.auth_service import login_user
 
 auth_router = APIRouter()
+@auth_router.post("/login")
+async def login(payload: UserLoginRequest, db: AsyncSession = Depends(get_db)):
+    result = await login_user(db, payload.username, payload.password)
 
-@auth_router.post('/login')
-def login():
-    return {'message':'api is successful'} 
+    if not result:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    return result
