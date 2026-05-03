99184a09368341bfaf9559087e3afb3a7ac37380
Uzair Ahmad <uzair3368@gmail.com>
2026-05-03 15:00:58 +0500

    seed_user: check if the org or user already exists

--- src/app/db/seed_user.py
+++ src/app/db/seed_user.py
@@ -1,32 +1,55 @@
 import asyncio
+from sqlalchemy import select
+
 from src.app.db.session import AsyncSessionLocal, engine
-from src.app.models import User, Organization
+from src.app.models import User, Organization,UserRole
 from src.app.core.security import hash_password
 from src.app.models.base import Base
 
+
 async def seed():
+    ORGANIZATION_NAME = "Test Org"
+    USERNAME = "test"
+    PASSWORD = "1234"
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
     async with AsyncSessionLocal() as db:
-        # 1. Create organization first
-        org = Organization(
-            name="Test Org"
-        )
-        db.add(org)
-        await db.flush()  # ensures org.id is available
 
-        # 2. Create user with organization_id
-        user = User(
-            username="test@example.com",
-            password=hash_password("1234"),
-            organization_id=org.id
+        # 🔹 1. Check if organization exists
+        org_result = await db.execute(
+            select(Organization).where(Organization.name == ORGANIZATION_NAME)
         )
+        org = org_result.scalar_one_or_none()
 
-        db.add(user)
+        if not org:
+            org = Organization(name=ORGANIZATION_NAME)
+            db.add(org)
+            await db.flush()  # get org.id
+            print("Organization created")
+        else:
+            print("Organization already exists")
+
+        # 🔹 2. Check if user exists
+        user_result = await db.execute(
+            select(User).where(User.username == USERNAME)
+        )
+        user = user_result.scalar_one_or_none()
+
+        if not user:
+            user = User(
+                username=USERNAME,
+                password=hash_password(PASSWORD),
+                organization_id=org.id,
+                role=UserRole.admin
+            )
+            db.add(user)
+            print("User created")
+        else:
+            print("User already exists")
 
         await db.commit()
 
-    print("Organization and User created")
 
-asyncio.run(seed())
+if __name__ == "__main__":
+    asyncio.run(seed())
