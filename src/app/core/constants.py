445b4220a9bcab527c9f595643bb568aae0e5335
Uzair Ahmad <uzair3368@gmail.com>
2026-05-03 22:23:22 +0500

    add access level constants and more

--- src/app/core/constants.py
+++ src/app/core/constants.py
@@ -1,3 +1,30 @@
+
+from enum import Enum
+from pathlib import Path
+import os
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 REFRESH_TOKEN_EXPIRE_MINUTES = 1400
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+UPLOADS_FOLDER = "uploads" 
+UPLOADS_PATH = Path(f"{BASE_DIR}/{UPLOADS_FOLDER}")
+UPLOADS_PATH.mkdir(exist_ok=True)
+
+ROLE_ACCESS = {
+    "employee": ["employee_only"],
+    "manager": ["employee_only", "manager_only"],
+    "admin": ["employee_only", "manager_only", "admin_only"],
+}
+
+ALLOWED_MIME_TYPES = [
+    "text/plain",
+    "application/pdf",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    ]
+
+class AccessLevel(str, Enum):
+    EMPLOYEE_ONLY = "employee_only"
+    MANAGER_ONLY = "manager_only"
+    ADMIN_ONLY = "admin_only"
