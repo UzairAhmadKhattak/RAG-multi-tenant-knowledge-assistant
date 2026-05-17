714391dd7ba6d6ccce3de19683dc69f685daded1
Uzair Ahmad <uzair3368@gmail.com>
2026-05-17 15:37:15 +0500

    refactor: update the upload api to update or create the document

--- src/app/api/v1/documents.py
+++ src/app/api/v1/documents.py
@@ -3,13 +3,13 @@ from src.app.core.constants import (UPLOADS_FOLDER,
                                     UPLOADS_PATH)
 from src.app.utils.docs_helpers import save_file
 from src.app.models import Document,User,UserRole
-from src.app.utils.query_helpers import get_or_create
+from src.app.utils.query_helpers import update_or_create
 from src.app.schemas.base import GeneralResponse
 from src.app.db.session import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.app.api.deps import get_current_user
 from src.app.services.embed import embed_file
-from fastapi import HTTPException
+from src.app.services.summarizer import summaries_document
 
 doc_router = APIRouter()
 
@@ -23,22 +23,21 @@ async def upload_doc(file: UploadFile = File(...),
     file_url = f"{UPLOADS_FOLDER}/{file.filename}"
     file_path = f"{UPLOADS_PATH}/{file.filename}"
     mime_type = await save_file(file_path=file_path,file=file)
+    document_summary = await summaries_document(file)
     organization_id = user.organization_id
     user_id = user.id
-    doc, created = await get_or_create(db, 
+    doc, created = await update_or_create(db, 
                                  Document, 
                                  title = title, 
                                  defaults={"file_path":file_url,
                                            "uploaded_by_id":user_id,
-                                           "organization_id":organization_id
+                                           "organization_id":organization_id,
+                                           'summary':document_summary
                                            }
                                 )
-    if created is False:
-        raise HTTPException(
-            status_code=400,
-            detail='Document with this title already exists'
-        )
-    await embed_file(db,file_path,mime_type,organization_id,role,doc.id)
+    await embed_file(db,file_path,mime_type,organization_id,role,doc.id,True)
     await db.commit()
     await db.close()
-    return {'detail':'Document successfully uploaded'}
+    if created:
+        return {'detail':'Document successfully uploaded'}
+    return {'detail':'Document successfully updated'}
